5eb42773911cb3a4bf43f4bfaa114819fc08104a
Dan <dgealow@paradigm4.com>
2024-01-15 21:27:04 +0000

    Initial commit

---
+++ R/dtaware.R
@@ -0,0 +1,28 @@
+#' @import data.table
+NULL
+
+#' Get the first row of a data.table
+#'
+#' Returns `dt[1]`, which for a data.table is the first row,
+#' but for a data.frame, is the first *column*
+#'
+#' @param dt a data.table
+#' @return `dt[1]`, the first row
+#' @export
+get_first_row_of_datatable = function(dt) {
+  return(dt[1])
+}
+
+#' Get the first column of a data.frame
+#'
+#' Gets the first column of a data.frame OR data.table,
+#' as a data.frame/table, by running `df[1]` with
+#' .datatable.aware = FALSE
+#'
+#' @param df a data.frame or data.table
+#' @return `df[1]`, the first column, as a data.frame/table
+#' @export
+get_first_column_of_dataframe = function(df) {
+  .datatable.aware = FALSE
+  return(df[1])
+}
